b1e907539ce1c61d1e86addc2c5f6ecfb5b511a7
Chin-Chang Yang <chin.chang.yang@gmail.com>
2014-05-11 11:06:49 +0800

    SPS-DE

---
+++ solver/soea/debest1bin_sps.m
@@ -0,0 +1,238 @@
+function [xmin, fmin, out] = debest1bin_sps(fitfun, lb, ub, maxfunevals, options)
+% DEBEST1BIN_SPS DE/BEST/1/BIN with SPS Framework
+% DEBEST1BIN_SPS(fitfun, lb, ub, maxfunevals) minimize the function fitfun in
+% box constraints [lb, ub] with the maximal function evaluations
+% maxfunevals.
+% DEBEST1BIN_SPS(..., options) minimize the function by solver options.
+if nargin <= 4
+	options = [];
+end
+
+defaultOptions.NP = 100;
+defaultOptions.F = 0.7;
+defaultOptions.CR = 0.5;
+defaultOptions.Q = 70;
+defaultOptions.Display = 'off';
+defaultOptions.RecordPoint = 100;
+defaultOptions.ftarget = -Inf;
+defaultOptions.TolStagnationIteration = Inf;
+defaultOptions.initial.X = [];
+defaultOptions.initial.f = [];
+
+options = setdefoptions(options, defaultOptions);
+F = options.F;
+CR = options.CR;
+Q = options.Q;
+isDisplayIter = strcmp(options.Display, 'iter');
+RecordPoint = max(0, floor(options.RecordPoint));
+ftarget = options.ftarget;
+TolStagnationIteration = options.TolStagnationIteration;
+
+D = numel(lb);
+
+if ~isempty(options.initial)
+	options.initial = setdefoptions(options.initial, defaultOptions.initial);
+	X = options.initial.X;
+	fx = options.initial.f;
+else
+	X = [];
+	fx = [];
+end
+
+if isempty(X)	
+	NP = options.NP;
+else
+	[~, NP] = size(X);
+end
+
+% Initialize variables
+counteval = 0;
+countiter = 1;
+countStagnation = 0;
+out = initoutput(RecordPoint, D, NP, maxfunevals, ...
+	'FC');
+
+% Initialize contour data
+if isDisplayIter
+	[XX, YY, ZZ] = advcontourdata(D, lb, ub, fitfun);
+end
+
+% Initialize population
+if isempty(X)
+	X = zeros(D, NP);
+	for i = 1 : NP
+		X(:, i) = lb + (ub - lb) .* rand(D, 1);
+	end
+end
+
+% Evaluation
+if isempty(fx)
+	fx = zeros(1, NP);
+	for i = 1 : NP
+		fx(i) = feval(fitfun, X(:, i));
+		counteval = counteval + 1;
+	end
+end
+
+% Sort
+[fx, fidx] = sort(fx);
+X = X(:, fidx);
+
+% Initialize variables
+V = X;
+U = X;
+fu = zeros(1, NP);
+FC = zeros(1, NP);		% Consecutive Failure Counter
+rt = zeros(1, NP);
+r1 = zeros(1, NP);
+r2 = zeros(1, NP);
+SP = X;
+fSP = fx;
+iSP = 1;
+
+% Display
+if isDisplayIter
+	displayitermessages(...
+		X, U, fx, countiter, XX, YY, ZZ);
+end
+
+% Record
+out = updateoutput(out, X, fx, counteval, countiter, ...
+	'FC', FC);
+
+% Iteration counter
+countiter = countiter + 1;
+
+while true
+	% Termination conditions
+	outofmaxfunevals = counteval > maxfunevals - NP;
+	reachftarget = min(fx) <= ftarget;
+	stagnation = countStagnation >= TolStagnationIteration;	
+	if outofmaxfunevals || reachftarget || stagnation
+		break;
+	end
+	
+	for i = 1 : NP
+		rt(i) = i;
+		
+		% Generate r1
+		r1(i) = floor(1 + NP * rand);
+		while rt(i) == r1(i)
+			r1(i) = floor(1 + NP * rand);
+		end
+		
+		% Generate r2
+		r2(i) = floor(1 + NP * rand);
+		while rt(i) == r2(i) || r1(i) == r2(i)
+			r2(i) = floor(1 + NP * rand);
+		end
+	end
+	
+	% Mutation
+	[~, ibestX] = min(fx);
+	[~, ibestSP] = min(fSP);
+	for i = 1 : NP	
+		if FC(i) <= Q
+			V(:, i) = X(:, ibestX) + F .* (X(:, r1(i)) - X(:, r2(i)));
+		else
+			V(:, i) = SP(:, ibestSP) + F .* (SP(:, r1(i)) - SP(:, r2(i)));
+		end
+	end
+	
+	for i = 1 : NP
+		% Binominal Crossover
+		jrand = floor(1 + D * rand);
+		if FC(i) <= Q
+			for j = 1 : D
+				if rand < CR || j == jrand
+					U(j, i) = V(j, i);
+				else
+					U(j, i) = X(j, rt(i));
+				end
+			end
+		else
+			for j = 1 : D
+				if rand < CR || j == jrand
+					U(j, i) = V(j, i);
+				else
+					U(j, i) = SP(j, rt(i));
+				end
+			end			
+		end
+	end
+	
+	% Correction for outside of boundaries
+	for i = 1 : NP
+		if FC(i) <= Q
+			for j = 1 : D
+				if U(j, i) < lb(j)
+					U(j, i) = 0.5 * (lb(j) + X(j, rt(i)));
+				elseif U(j, i) > ub(j)
+					U(j, i) = 0.5 * (ub(j) + X(j, rt(i)));
+				end
+			end
+		else
+			for j = 1 : D
+				if U(j, i) < lb(j)
+					U(j, i) = 0.5 * (lb(j) + SP(j, rt(i)));
+				elseif U(j, i) > ub(j)
+					U(j, i) = 0.5 * (ub(j) + SP(j, rt(i)));
+				end
+			end
+		end
+	end
+	
+	% Display
+	if isDisplayIter
+		displayitermessages(...
+			X, U, fx, countiter, XX, YY, ZZ);
+	end
+	
+	% Evaluation
+	for i = 1 : NP
+		fu(i) = feval(fitfun, U(:, i));
+		counteval = counteval + 1;
+	end
+	
+	% Selection
+	FailedIteration = true;
+	for i = 1 : NP		
+		if fu(i) < fx(i)
+			X(:, i)		= U(:, i);
+			fx(i)		= fu(i);
+			SP(:, iSP)	= U(:, i);
+			fSP(iSP)	= fu(i);
+			iSP			= mod(iSP, NP) + 1;
+			FailedIteration = false;
+			FC(i)		= 0;
+		else
+			FC(i) = FC(i) + 1;
+		end
+	end
+	
+	% Sort	
+	[fx, fidx] = sort(fx);
+	X = X(:, fidx);
+	FC = FC(fidx);
+	
+	% Record
+	out = updateoutput(out, X, fx, counteval, countiter, ...
+		'FC', FC);
+	
+	% Iteration counter
+	countiter = countiter + 1;
+	
+	% Stagnation iteration
+	if FailedIteration
+		countStagnation = countStagnation + 1;
+	else
+		countStagnation = 0;
+	end	
+end
+
+[fmin, minindex] = min(fx);
+xmin = X(:, minindex);
+
+out = finishoutput(out, X, fx, counteval, countiter, ...
+	'FC', zeros(NP, 1));
+end
